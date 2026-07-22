e10bbf7a03f8353271d40c4c4813219cdfdef478
pandara-tech <mentis805@gmail.com>
2026-07-22 09:36:04 +0300

    Fix: images broken - add assetPrefix, loader, trailingSlash, and webpack aliases

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,31 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: false,
   },
   basePath: '/divagym',
+  assetPrefix: '/divagym',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
+    ],
+    loader: 'custom',
+    path: '/divagym/_next/image',
   },
   output: 'export',
+  trailingSlash: true,
   transpilePackages: ['motion'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './app'),
+      '@/app': require('path').resolve(__dirname, './app'),
+      '@/components': require('path').resolve(__dirname, './app'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
